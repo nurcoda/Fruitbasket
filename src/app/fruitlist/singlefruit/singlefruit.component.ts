5718cdc274f7eeebf88bc79fee8e8c570589b2f3
nurcoda <mailo.mittelaedt@googlemail.com>
2024-07-24 12:32:34 +0200

    singlefruit out source

--- src/app/fruitlist/singlefruit/singlefruit.component.ts
+++ src/app/fruitlist/singlefruit/singlefruit.component.ts
@@ -1,4 +1,4 @@
-import { Component, input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-singlefruit',
@@ -8,14 +8,14 @@ import { Component, input } from '@angular/core';
   styleUrl: './singlefruit.component.scss'
 })
 export class SinglefruitComponent {
-  @input() fruit = 0; {
-  name: "Apfel",
+  @Input() fruit = {
+    name: "Apfel",
     img: "apple.png",
-      description: "Äpfel sind aufgrund ihres hohen Wassergehalts kalorienarm und enthalten nur Spuren von Fett und Eiweiß, dafür aber rund zwei Prozent Ballaststoffe und etwa elf Prozent Kohlenhydrate. Äpfel enthalten auch viele Vitamine und Mineralstoffe und sind daher eine wichtige Quelle für uns - zum Beispiel für Vitamin C.",
-        genus: "Kernobstgewächsen innerhalb der Familie der Rosengewächse",
-          stars: 2.3,
-            reviews: [{ name: "Kevin W.", text: "ist lecker" }, { name: "Arne P.", text: "nicht so meins" }],
+    description: "Äpfel sind aufgrund ihres hohen Wassergehalts kalorienarm und enthalten nur Spuren von Fett und Eiweiß, dafür aber rund zwei Prozent Ballaststoffe und etwa elf Prozent Kohlenhydrate. Äpfel enthalten auch viele Vitamine und Mineralstoffe und sind daher eine wichtige Quelle für uns - zum Beispiel für Vitamin C.",
+    genus: "Kernobstgewächsen innerhalb der Familie der Rosengewächse",
+    stars: 2.3,
+    reviews: [{ name: "Kevin W.", text: "ist lecker" }, { name: "Arne P.", text: "nicht so meins" }],
   }
 
-  
+
 }
