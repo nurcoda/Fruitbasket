ddb46ecf785716942f0b95a3c14dcbc55680bc3d
nurcoda <mailo.mittelaedt@googlemail.com>
2024-07-24 09:50:38 +0200

    first push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { FruitlistComponent } from './fruitlist/fruitlist.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, FruitlistComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
